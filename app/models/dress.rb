007aedaca5cce1222747bc57b45830e300d7288c
Kate Kilian Vovk <kathrynjkilian@gmail.com>
2015-02-26 20:46:59 -0700

    Added associations to Llamas/Dresses models

--- app/models/dress.rb
+++ app/models/dress.rb
@@ -2,4 +2,6 @@ class Dress < ActiveRecord::Base
 
   validates :style, :color, :material, :price, presence: true
 
+  belongs_to :llama
+
 end
--- app/models/llama.rb
+++ app/models/llama.rb
@@ -2,4 +2,6 @@ class Llama < ActiveRecord::Base
 
   validates :name, :color, presence: true
 
+  has_many :dresses
+
 end
